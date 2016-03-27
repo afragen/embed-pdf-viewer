3cfd61a85506940b24efdcb05234b3aade100f75
Andy Fragen <andy@thefragens.com>
2016-03-27 12:24:12 -0700

    fix branch

--- embed-pdf-viewer.php
+++ embed-pdf-viewer.php
@@ -9,7 +9,7 @@
  * License:           GPLv2+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
  * GitHub Plugin URI: https://github.com/afragen/embed-pdf-viewer
- * GitHub Branch:     develop
+ * GitHub Branch:     master
  */
 
 /**
