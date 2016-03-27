4901d85c8608b7c87559d3b083d090588f53cc4a
Andy Fragen <andy@thefragens.com>
2016-03-27 12:12:34 -0700

    urlencode link

--- embed-pdf-viewer.php
+++ embed-pdf-viewer.php
@@ -60,7 +60,7 @@ class Embed_PDF_Viewer {
 		$attachment = get_post( $id );
 		$out        = "";
 		if ( 'application/pdf' === $attachment->post_mime_type ) {
-			$out .= '<iframe src="https://docs.google.com/gview?url=' . $attachment->guid . '&embedded=true" style="width:100%; height:500px;" frameborder="0"></iframe>';
+			$out .= '<iframe src="https://docs.google.com/gview?url=' . urlencode( $attachment->guid ) . '&embedded=true" style="width:100%; height:500px;" frameborder="0"></iframe>';
 		}
 
 		return $out . $html . "\n\n";
