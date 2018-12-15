044c03262f83428bbcd8c0dde5dd7c47791b8303
Andy Fragen <andy@thefragens.com>
2018-12-15 10:50:31 -0800

    build

--- blocks/index.js
+++ blocks/index.js
@@ -10,107 +10,114 @@ const { isBlobURL } = wp.blob;
 
 const defaultWidth = 600;
 const defaultHeight = 400;
-const isExternalPDF = ( id, url ) => url && ! id && ! isBlobURL( url );
+const isExternalPDF = (id, url) => url && !id && !isBlobURL(url);
 
-const renderEmbed = ( props ) => {
+const renderEmbed = (props) => {
 	const { attributes: { url, width, height }, className } = props;
 	const style = { width, height };
-	if ( undefined === url || ! url ) {
+	if (undefined === url || !url) {
 		return null;
 	}
-	if ( undefined === style.width ) {
+	if (undefined === style.width) {
 		style.width = defaultWidth;
 	}
-	if ( undefined === style.height ) {
+	if (undefined === style.height) {
 		style.height = defaultHeight;
 	}
 	return (
-		<div className={ `${ className }__content-wrapper` }>
+		<div className={`${className}__content-wrapper`}>
 			<object
 				class="embed-pdf-viewer"
-				data={ url + '#scrollbar=1&toolbar=1' }
+				data={url + '#scrollbar=1&toolbar=1'}
 				type="application/pdf"
-				height={ style.height }
-				width={ style.width }
+				height={style.height}
+				width={style.width}
 			>
 				<iframe
-				class="embed-pdf-viewer"
-				src={ 'https://docs.google.com/viewer?url=' + encodeURIComponent( url ) + '&embedded=true' }
-				frameborder="0"
-				style={ {height: style.height, width: style.width } }
+					class="embed-pdf-viewer"
+					src={'https://docs.google.com/viewer?url=' + encodeURIComponent(url) + '&embedded=true'}
+					frameborder="0"
+					style={{ height: style.height, width: style.width }}
 				>
 				</iframe>
 			</object>
+			<iframe
+				class="embed-pdf-viewer"
+				src={'https://docs.google.com/viewer?url=' + encodeURIComponent(url) + '&embedded=true'}
+				frameborder="0"
+				style={{ height: style.height, width: style.width }}
+			>
+			</iframe>
 		</div>
 	);
 }
 
-const renderEdit = ( props ) => {
+const renderEdit = (props) => {
 	const { attributes: { id, url, width, height, align }, setAttributes, isEditing, hasError, setState, className, media, noticeUI, noticeOperations, toggleSelection, isRTL } = props;
-	const isExternal = isExternalPDF( id, url );
+	const isExternal = isExternalPDF(id, url);
 
-	function updateAttribute( key ) {
-		return function( value ) {
+	function updateAttribute(key) {
+		return function (value) {
 			const attr = {};
 			attr[key] = value;
-			setAttributes( attr );
+			setAttributes(attr);
 		}
 	}
 
-	function onSelectFile( media ) {
-		if ( media && media.url ) {
-			setState( {
+	function onSelectFile(media) {
+		if (media && media.url) {
+			setState({
 				hasError: false,
-			} );
-			setAttributes( {
+			});
+			setAttributes({
 				url: media.url,
 				id: media.id,
-			} );
+			});
 		}
 	}
 
 	function toggleIsEditing() {
-		setState( {
-			isEditing: ! isEditing,
-		} );
+		setState({
+			isEditing: !isEditing,
+		});
 	}
 
-	if ( undefined === url || ! url || hasError || isEditing ) {
+	if (undefined === url || !url || hasError || isEditing) {
 		return (
 			<MediaPlaceholder
 				icon="media-document"
-				labels={ {
-					title: __( 'PDF' ),
-					instructions: __( 'Drag a PDF, upload a new one or select a PDF from your library.' ),
-				} }
-				onSelect={ onSelectFile }
-				onSelectURL={ updateAttribute( 'url' ) }
-				notices={ props.noticeUI }
-				onError={ props.noticeOperations.createErrorNotice }
+				labels={{
+					title: __('PDF'),
+					instructions: __('Drag a PDF, upload a new one or select a PDF from your library.'),
+				}}
+				onSelect={onSelectFile}
+				onSelectURL={updateAttribute('url')}
+				notices={props.noticeUI}
+				onError={props.noticeOperations.createErrorNotice}
 				accept="application/pdf"
 			/>
 		);
 	}
 
 
-	const classes = classnames( className, {
-		'is-transient': isBlobURL( url ),
-	} );
+	const classes = classnames(className, {
+		'is-transient': isBlobURL(url),
+	});
 
 
 	let showRightHandle = false;
 	let showLeftHandle = false;
 
 	// See block-library/src/image/edit.js
-	if ( align === 'center' ) {
+	if (align === 'center') {
 		// When the image is centered, show both handles.
 		showRightHandle = true;
 		showLeftHandle = true;
-	} else if ( isRTL ) {
+	} else if (isRTL) {
 		// In RTL mode the image is on the right by default.
 		// Show the right handle and hide the left handle only when it is aligned left.
 		// Otherwise always show the left handle.
-		if ( align === 'left' ) {
+		if (align === 'left') {
 			showRightHandle = true;
 		} else {
 			showLeftHandle = true;
@@ -118,7 +125,7 @@ const renderEdit = ( props ) => {
 	} else {
 		// Show the left handle and hide the right handle only when the image is aligned right.
 		// Otherwise always show the right handle.
-		if ( align === 'right' ) {
+		if (align === 'right') {
 			showLeftHandle = true;
 		} else {
 			showRightHandle = true;
@@ -128,21 +135,21 @@ const renderEdit = ( props ) => {
 	return (
 		<Fragment>
 			<InspectorControls>
-				<PanelBody title={ __( 'Embed size' ) } initialOpen={ true }>
+				<PanelBody title={__('Embed size')} initialOpen={true}>
 					<div>
 						<TextControl
 							type="number"
-							min={ 20 }
-							label={ __( 'Width' ) }
-							value={ undefined === width ? defaultWidth : width }
-							onChange={ updateAttribute( 'width' ) }
+							min={20}
+							label={__('Width')}
+							value={undefined === width ? defaultWidth : width}
+							onChange={updateAttribute('width')}
 						/>
 						<TextControl
 							type="number"
-							label={ __( 'Height' ) }
-							value={ undefined === height ? defaultHeight : height }
-							min={ 1 }
-							onChange={ updateAttribute( 'height' ) }
+							label={__('Height')}
+							value={undefined === height ? defaultHeight : height}
+							min={1}
+							onChange={updateAttribute('height')}
 						/>
 					</div>
 				</PanelBody>
@@ -150,36 +157,36 @@ const renderEdit = ( props ) => {
 
 			<BlockControls>
 				<BlockAlignmentToolbar
-					value={ align }
-					onChange={ updateAttribute( 'align') }
+					value={align}
+					onChange={updateAttribute('align')}
 				/>
 				<Toolbar>
-					{ isExternal && (
+					{isExternal && (
 						<IconButton
 							className="components-icon-button components-toolbar__control"
-							label={ __( 'Edit PDF' ) }
-							onClick={ toggleIsEditing }
+							label={__('Edit PDF')}
+							onClick={toggleIsEditing}
 							icon="edit"
-							/>
-					) }
-					{ ! isExternal && (
-					<MediaUpload
-						onSelect={ onSelectFile }
-						value={ id }
-						render={ ( { open } ) => (
-							<IconButton
-								className="components-toolbar__control"
-								label={ __( 'Edit PDF' ) }
-								onClick={ open }
-								icon="edit"
-							/>
-						) }
-					/>
-					) }
+						/>
+					)}
+					{!isExternal && (
+						<MediaUpload
+							onSelect={onSelectFile}
+							value={id}
+							render={({ open }) => (
+								<IconButton
+									className="components-toolbar__control"
+									label={__('Edit PDF')}
+									onClick={open}
+									icon="edit"
+								/>
+							)}
+						/>
+					)}
 				</Toolbar>
 			</BlockControls>
 
-			<div className={ classes }>
+			<div className={classes}>
 				<ResizableBox
 					size={
 						width && height ? {
@@ -188,49 +195,49 @@ const renderEdit = ( props ) => {
 						} : undefined
 					}
 					lockAspectRatio
-					enable={ {
+					enable={{
 						top: false,
 						right: showRightHandle,
 						bottom: true,
 						left: showLeftHandle,
-					} }
-					onResizeStart={ () => {
-						toggleSelection( false );
-					} }
-					onResizeStop={ ( event, direction, elt, delta ) => {
-						setAttributes( {
-							width: parseInt( currentWidth + delta.width, 10 ),
-							height: parseInt( currentHeight + delta.height, 10 ),
-						} );
-						toggleSelection( true );
-					} }
+					}}
+					onResizeStart={() => {
+						toggleSelection(false);
+					}}
+					onResizeStop={(event, direction, elt, delta) => {
+						setAttributes({
+							width: parseInt(currentWidth + delta.width, 10),
+							height: parseInt(currentHeight + delta.height, 10),
+						});
+						toggleSelection(true);
+					}}
 				>
-					{ renderEmbed( props ) }
+					{renderEmbed(props)}
 				</ResizableBox>
 			</div>
 		</Fragment>
 	);
 };
 
-registerBlockType( 'embed-pdf-viewer/embed-pdf-viewer', {
-	title: __( 'PDF' ),
+registerBlockType('embed-pdf-viewer/embed-pdf-viewer', {
+	title: __('PDF'),
 	icon: 'media-document',
 	category: 'embed',
 	attributes: {
-		id:  { type: 'number', },
-		url:    { type: 'string', },
-		width:  { type: 'number', },
+		id: { type: 'number', },
+		url: { type: 'string', },
+		width: { type: 'number', },
 		height: { type: 'number', },
-		align:  { type: 'string', },
+		align: { type: 'string', },
 	},
 
-	getEditWrapperProps( attributes ) {
+	getEditWrapperProps(attributes) {
 		const { align, width } = attributes;
-		if ( 'left' === align || 'center' === align || 'right' === align ) {
-			return { 'data-align': align, 'data-resized': !! width };
+		if ('left' === align || 'center' === align || 'right' === align) {
+			return { 'data-align': align, 'data-resized': !!width };
 		}
 	},
 
-	edit: withNotices( withState( { isEditing: false, hasError: false })( renderEdit ) ),
+	edit: withNotices(withState({ isEditing: false, hasError: false })(renderEdit)),
 	save: renderEmbed,
-} );
+});
