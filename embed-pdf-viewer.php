d1865bd6914feb8a3693a6e6457da3af407e5ee6
Andy Fragen <andy@thefragens.com>
2024-07-30 21:09:40 -0700

    bump

--- embed-pdf-viewer.php
+++ embed-pdf-viewer.php
@@ -14,7 +14,7 @@
  * Description:       Embed a PDF from the Media Library or elsewhere via oEmbed or as a block into an `iframe` tag.
  * Author:            Andy Fragen
  * Author URI:        https://github.com/afragen
- * Version:           2.3.1.1
+ * Version:           2.3.1.2
  * License:           GPLv2+
  * Domain Path:       /languages
  * Text Domain:       embed-pdf-viewer
@@ -133,7 +133,7 @@ class Embed_PDF_Viewer {
 		global $is_chrome;
 
 		$url = $attributes['url'] ?? null;
-					if ( ! $url ) {
+		if ( ! $url ) {
 			return '';
 		}
 
